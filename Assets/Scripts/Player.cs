51e1d0cd15bc8e906eab1e21f3a5b480feb644bb
syt1209 <syt1209@gmail.com>
2020-09-18 13:41:36 -0400

    Player movement created.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    [SerializeField]
+    private float _speed = 3.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position and start at a new position
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        // Get moving direction input from the User, using Input manager
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
+
+        // Translate the object using transform.Translate
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 0), 0);
+
+        // Wrap the player game object in the X direction
+        if (transform.position.x > 9.6f)
+        {
+            transform.position = new Vector3 (-9.6f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.6f)
+        {
+            transform.position = new Vector3(9.6f, transform.position.y, 0);
+        }
+    }
+
+}
