df73032347fc6838ba21660db822a47fa844b431
syt1209 <syt1209@gmail.com>
2020-09-24 14:26:43 -0400

    UI-Flickering Game Over text added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,9 +13,13 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Sprite[] _livesSprites;
 
+    [SerializeField]
+    private Text _gameOvertxt;
+
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOvertxt.gameObject.SetActive(false);
 
     }
 
@@ -27,5 +31,23 @@ public class UIManager : MonoBehaviour
     public void CurrentLife(int playerLife)
     {
         _livesImg.sprite = _livesSprites[playerLife];
+
+        if (playerLife < 1)
+        {
+            _gameOvertxt.gameObject.SetActive(true);
+
+            StartCoroutine(GameOverFlickerRoutine());
+        }
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true) 
+        {
+            _gameOvertxt.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOvertxt.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
