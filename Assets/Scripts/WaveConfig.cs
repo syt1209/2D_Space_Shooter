27f52bad9c7b8f11c9d7a1d26a5096b70fd7f00d
syt1209 <syt1209@gmail.com>
2020-11-30 16:41:17 -0500

    Phase II-New Enemy Movement (Three movement paths created)

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Enemy Movement Configuration")]
+public class WaveConfig : ScriptableObject {
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _pathPrefab;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private int _numOfEnemies = 5;
+    [SerializeField] private float _spawnInterval = 0.5f;
+}
