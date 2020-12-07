698fdd0a933cd38249cb84efce84241be8a81d94
syt1209 <syt1209@gmail.com>
2020-12-07 12:09:08 -0500

    Phase II-Pick Collect-powerup move to player when press 'C'

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -11,24 +11,38 @@ public class Powerups : MonoBehaviour
 
     private Player _player;
 
+    private Transform _playerTrans;
+
     [SerializeField]
     private AudioClip _audioClip;
+
+    private bool _collected;
 
     
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        _playerTrans = GameObject.Find("Player").GetComponent<Transform>();
+        _collected = false;
 
         if (_player == null)
         {
             Debug.LogError("Player is NULL.");
         }
+
+        if (_playerTrans == null)
+        { Debug.LogError("Player transform is NULL."); }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_collected is false && Input.GetKeyDown(KeyCode.C)) 
+        {
+            AutoCollect();
+        }
+
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
         if (transform.position.y < -6.0f)
@@ -71,6 +85,18 @@ public class Powerups : MonoBehaviour
             }
 
             Destroy(this.gameObject, 0.2f);
+        }
+    }
+
+    private void AutoCollect()
+    {
+        while (_collected is false)
+        { 
+            transform.position = Vector3.MoveTowards(transform.position, _playerTrans.position, Time.deltaTime);
+            if (transform.position == _playerTrans.position)
+            {
+                _collected = true;
+            }
         }
     }
 }
