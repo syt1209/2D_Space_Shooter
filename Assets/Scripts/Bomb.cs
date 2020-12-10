4e2373162ac6f73eddf7d32aec56f1f0a2871e7f
syt1209 <syt1209@gmail.com>
2020-12-10 12:58:00 -0500

    Phase II-New Enemy-Heat seeking bomb following player added

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10.0f;
+
+    private Transform _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _target = GameObject.Find("Player").GetComponent<Transform>();
+        transform.LookAt(_target);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Player player = GameObject.Find("Player").GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
